3c1c8060c8a934896b7843fbea53bb848b5028b0
rekky1aws <killikomfr@gmail.com>
2023-08-09 15:40:16 +0200

    Guessing game clean version

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,9 +7,10 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     loop {
+        println!(" -----");
         println!("Please input your guess.");
 
         let mut guess = String::new();
@@ -18,16 +19,19 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-        println!("You guessed: {guess}");
+        // println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!(" > Too small!"),
+            Ordering::Greater => println!(" > Too big!"),
             Ordering::Equal => 
             {
-                println!("You win");
+                println!(" > You win");
                 break;
             }
         }
