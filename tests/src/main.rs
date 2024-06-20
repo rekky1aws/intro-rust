4dc3cc2a16b9b287137ed13035302f46e4f5b50b
rekky1aws <killikomfr@gmail.com>
2024-06-20 01:54:21 +0100

    Playing with variables and behavior

---
+++ tests/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+
+    x = 6;
+    println!("The value of x is: {x}");
+
+
+    let mut y: u8 = 254;
+    let z: Option<u8> = y.checked_add(1);
+
+    y += 1;
+    y = y.wrapping_add(1);
+    println!("{:?}, {:?}", y, z);
+
+
+    let arr: [u8; 5] = [0; 5];
+    println!("{:?}", arr);
+}
