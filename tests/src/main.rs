1fb3589adb5d727cd564090d8e336455a707656b
rekky1aws <killikomfr@gmail.com>
2024-06-23 17:14:27 +0200

    Loop with labels

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -61,4 +61,26 @@ fn main() {
 
     println!("The result is {result}");
 
+
+    // Labelling loops
+    println!("\n\n\n\n\n");
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}", );
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
